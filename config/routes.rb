44abc0b3047004310f83dbfdc63769daacc518a3
Erez Bosch <erezaka@gmail.com>
2015-07-18 18:29:26 -0700

    finishing touches

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ AuthDemo::Application.routes.draw do
   get '/home', to: "static_pages#home"
   get '/contact', to: "static_pages#contact"
   get '/about', to: "static_pages#about"
+  root to: "static_pages#home"
 end
